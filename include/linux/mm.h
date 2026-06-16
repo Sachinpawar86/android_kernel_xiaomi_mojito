f270a19bfd77f570bdf9f874b12ffa18e0007eda
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-06-16 19:22:21 +0800

    Revert "mm: make faultaround produce old ptes"
    This reverts commit ad50bf626a15be944019a296ed70489bbd49b8bb.

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -338,7 +338,6 @@ extern pgprot_t protection_map[16];
 #define FAULT_FLAG_INSTRUCTION  0x100	/* The fault was during an instruction fetch */
 /* Speculative fault, not holding mmap_sem */
 #define FAULT_FLAG_SPECULATIVE	0x200
-#define FAULT_FLAG_PREFAULT_OLD 0x400   /* Make faultaround ptes old */
 
 #define FAULT_FLAG_TRACE \
 	{ FAULT_FLAG_WRITE,		"WRITE" }, \
@@ -2949,8 +2948,6 @@ void __init setup_nr_node_ids(void);
 #else
 static inline void setup_nr_node_ids(void) {}
 #endif
-
-extern int want_old_faultaround_pte;
 
 #endif /* __KERNEL__ */
 #endif /* _LINUX_MM_H */
